b485eecfc1b7e16314e0daf8c4608981652017d3
Anthony Prancl <aprancl1597@gmail.com>
2022-06-20 19:55:02 -0400

    Major changes to person sub menu; need to fix writting data to file

--- nutrition.py
+++ nutrition.py
@@ -1,4 +1,4 @@
-
+import datetime 
 import pdb
 import sys
 import os
@@ -96,44 +96,88 @@ def main_menu(ppl):
 
 
 def person_menu(ppl):
+    date = datetime.datetime.now()
     #get person data
     idx = int(input("Enter the your person's index: "))
     person = ppl[idx - 1]
-    person_data = get_data(person)
+    #labels
     nut_categories = ["Calories", "Protein", "Carbohydrates", "Fat", "Sugar Limit", "Saturated Fat Limit"]
+    # for totals
+    person_data = get_data(person)
+    # for daily
+    person_data_day = get_data_day(person)
+    
 
 
     # display that person's information
-    print("-" * 40)
-    print("|            Macronutrients            |")
-    print("-" * 40)
-    for i in range(len(person_data)): 
-        print("|    {0:<19} : {1:^12s}|".format(nut_categories[i], person_data[i]))
-    print("-" * 40, end='\n\n')
 
+    # some totals
+    print("-" * 40)
+    print("|{0:^38s}|".format("Daily Macro Goals"))
+    print("|{0:^38s}|".format('(' + person + ')'))
+    draw_chart(nut_categories, person_data)
+    
+    # daily progress
+    print("-" * 40)
+    print("|{0:^38s}|".format("Daily Progress"))
+    print("|{0:^38s}|".format('(' + date.strftime("%A") + ')')) # the day of the week
+    print("| {0:^37s}|".format(str(date)[0:11]))
+
+    print('-' * 40)
+    for i in range(len(person_data_day)):
+        print("|    {0:<19} : {1:^12s}| ---{2:.2f}%".format(nut_categories[i], person_data_day[i] + '/' + person_data[i], float(person_data_day[i]) / float(person_data[i])))
+    print('-' * 40)
+    
     # display some more options
 
     while True:    
 
-        print("More options...\nSee current day (1)\nReset day (2)\nBack (4)")
+        print("More options...\nAdd Meal Entry (1)\nReset day (2)\nBack (3)")
 
         option = int(input("ENTER: "))
-        while int(option) < 1 or int(option) > 4:
+        while int(option) < 1 or int(option) > 3:
             print("Please enter an option between 1 and 5")
             option = input("ENTER: ")
 
         if option == 1:
-            #cur_dat_menu(person)
-            pass
+            add_meal(person)
+
+            print("-" * 40)
+            print("|{0:^38s}|".format("Daily Progress"))
+            print("|{0:^38s}|".format('(' + date.strftime("%A") + ')')) # the day of the week
+            print("| {0:^37s}|".format(str(date)[0:11]))
+
+            print('-' * 40)
+            for i in range(len(person_data_day)):
+                print("|    {0:<19} : {1:^12s}| ---{2:.2f}%".format(nut_categories[i], person_data_day[i] + '/' + person_data[i], float(person_data_day[i]) / float(person_data[i])))
+            print('-' * 40)
+
+
         elif option == 2:
             #reset_day(person)
             pass
         elif option == 3:
+            display_ppl(ppl)
             return
 
-    
+def cur_day_menu(person):
 
-        
+
+
+
+    pass
+
+
+def draw_chart(labels, data):
+
+    print("-" * 40)
+    for i in range(len(data)):
+        print("|    {0:<19} : {1:^12s}|".format(labels[i], data[i]))
+
+    print("-" * 40, end='\n')
+
+
+
 def register_person(ppl):
 
     # gather data from user 
@@ -156,10 +200,34 @@ def register_person(ppl):
     out_file.write('carbs = ' + str(carbs) + '\n')
     out_file.write('fat = ' + str(fat) + '\n')
     out_file.write('sugar = ' + str(lim_sugar) + '\n')
-    out_file.write('satfat = ' + str(lim_satfat) + '\n\n')
+    out_file.write('satfat = ' + str(lim_satfat) + '\n')
+
+    out_file.write('calories_day = ' + '0' + '\n' )
+    out_file.write('protein_day = ' + '0' + '\n' )
+    out_file.write('carbs_day = ' + '0' + '\n')
+    out_file.write('fat_day = ' + '0' + '\n')
+    out_file.write('sugar_day = ' + '0' + '\n')
+    out_file.write('satfat_day = ' + '0' + '\n\n')
     out_file.close()
 
-    
+def add_meal(person):
+
+
+    # gather data from user 
+    print('---Enter the nutritional value of your meal---')
+    print("\namout of...")
+    calories = int(input("Calories: "))
+    protein = int(input("Protein: "))
+    carbs = int(input("Carbs: "))
+    fat = int(input("Fat: "))
+    sugar = int(input("Sugar: "))
+    satfat = int(input("Saturated Fat: "))
+
+    data = [calories, protein, carbs, fat, sugar, satfat]
+
+    #Write to file
+    set_data_day(person, data)
+
 
 
 def wipe_all_data(ppl):
@@ -204,7 +272,43 @@ def get_data(person):
                 data.append(lines[i].split()[2])
                 i += 1
             return data
-    
+
+def get_data_day(person):
+
+    data = []
+
+    out_file = open('data.txt', 'r')
+    lines = out_file.readlines()
+
+    for i in range(len(lines)):
+        line = lines[i].split()
+
+        if len(line) > 0 and line[2].rstrip('\n') == person:
+            i += 7
+            for j in range(6):
+                data.append(lines[i].split()[2])
+                i += 1
+            return data
+
+
+
+
+def set_data_day(person, data):
+
+    out_file = open('data.txt', 'r')
+    lines = out_file.readlines()
+
+    for i in range(len(lines)):
+        line = lines[i].split()
+
+        if len(line) > 0 and line[2].rstrip('\n') == person:
+            i += 7
+            for j in range(6):
+                line[2] = data[j]
+                i += 1
+            break
+
+    out_file.close()
    
 
 
